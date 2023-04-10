0978fa695d84707c6267c34e41e5b1fce3fd74ec
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2023-04-10 22:49:30 +1200

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+const App: FC = () => {
+  return (
+    <div className="app light">
+      <header>
+        <h1>GitHub Profile Search</h1>
+      </header>
+      <main>
+      </main>
+    </div>
+  )
+}
+
+export default App
