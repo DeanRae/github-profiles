093b73f23053d4a5f9ce364d85f5c21a73abbcb2
Deanne Alabastro <alabasdean@ecs.vuw.ac.nz>
2023-04-11 16:12:18 +1200

    update components to reflect new interfaces

--- src/components/ProfileResult/ProfileResult.tsx
+++ src/components/ProfileResult/ProfileResult.tsx
@@ -1,51 +1,33 @@
 import { FC } from "react"
+import { GitHubProfile } from "../../api/githubProfileSearch";
 
-export interface RepositoryResult {
-    name: string;
-    url: string;
-    description: string;
-    topics: string[];
-}
-
-export interface ProfileResult {
-    username: string;
-    name: string;
-    avatar: string;
-    url: string;
-    numFollowers: number;
-    repositories: RepositoryResult[];
-}
-
-const ProfileResult: FC<ProfileResult> = ({
-    username,
+const ProfileResult: FC<GitHubProfile> = ({
+    login,
     name,
-    avatar,
-    url,
-    numFollowers,
-    repositories
+    avatar_url,
+    html_url,
+    followers,
+    repositoryCount,
+    repositories  
 }) => {
     return (
         <div className="profile-search-result">
             <div className="profile-search-result__profile">
-                <img src={avatar} alt={`${name}'s avatar`} />
+                <img src={avatar_url} alt={`${name}'s avatar`} />
                 <h2>
                     <span>{name}</span>
-                    <span><a href={url} target="_blank">{username}</a></span>
+                    <span><a href={html_url} target="_blank">{login}</a></span>
                 </h2>
-                <p><span>{numFollowers}</span> followers</p>
+                <p><span>{followers}</span> followers</p>
+                <p><span>{repositoryCount}</span> repositories</p>
             </div>
             <div className="profile-search-result__repositories">
                 <h3>Latest Repositories</h3>
                 {!repositories?.length && <p>{name} has no repositories</p>}
-                {repositories?.map(({url, name, topics, description}) => 
-                    <div className="profile-search-result__repo" key={url}>
-                        <h2><a href={url} target="_blank">{name}</a></h2>
+                {repositories?.map(({id, html_url, name, description}) => 
+                    <div className="profile-search-result__repo" key={id}>
+                        <h2><a href={html_url} target="_blank">{name}</a></h2>
                         <p>{description}</p>
-                        {topics?.length && (
-                            <ul>
-                                {topics.map(topic => <li key={topic}>{topic}</li>)}
-                            </ul>
-                        )}
                     </div>
                 )}
             </div>
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,26 @@
 import { FC, FormEvent } from 'react'
 
+interface FormElements extends HTMLFormControlsCollection {
+    searchInput: HTMLInputElement
+}
+export interface SearchBarFormElement extends HTMLFormElement {
+    readonly elements: FormElements
+}
 interface Props {
     inputId: string;
     label: string;
-    onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+    onSubmit: (e: FormEvent<SearchBarFormElement>) => void;
+    isLoading: boolean;
 }
-const SearchBar: FC<Props> = ({ inputId, label, onSubmit }) => {
+
+const SearchBar: FC<Props> = ({ inputId, label, onSubmit, isLoading }) => {
     return (
         <form role="search" onSubmit={onSubmit} className="search-bar">
             <label htmlFor={inputId} className="search-bar__label">
                 {label}
-                <input id={inputId} type="text" className="search-bar__input" />
+                <input id={inputId} type="text" className="search-bar__input" name={inputId}/>
             </label>
-            <button type="submit" className="search-bar__submit">
+            <button type="submit" className="search-bar__submit" disabled={isLoading}>
                 Search
             </button>
         </form>
